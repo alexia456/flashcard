10a86ed39d9560f151f95e9da4686b15b722fbe9
alexia456 <alexiadelfinodeoliveira30@gmail.com>
2025-05-20 15:03:49 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'historia',
+    'O que foi o imperialismo no séc xix?',
+    'Dominio de países europeus sobre territorios na áfrica,ásia e oceania'
+)
+
+criaCartao(
+    'historia',
+    'Quais países lideram o imperialismo?',
+    'Reino Unido, França, Alemanha, Belgica,Itália,Eua e Japãos'
+)
+
+criaCartao(
+    'historia',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'historia',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'historia',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+criaCartao(
+    'historia',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
